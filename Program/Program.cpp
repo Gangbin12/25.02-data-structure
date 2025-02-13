34e5978a32090d85b562357805e541336b845b98
Gangbin12 <rudqls2023@naver.com>
2025-02-13 19:41:54 +0900

    25.02.13 단일 연결 리스트

---
+++ Program/Program.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class SingleLinkedList
+{
+private:
+    int size;
+
+    struct Node
+    {
+        T data;
+        Node* next;
+    };
+
+    Node* head;
+public:
+
+    SingleLinkedList()
+    {
+        size = 0;
+        head = nullptr;
+        currentNode 
+    }
+        
+    void push_front(T data)
+    {
+        Node* newNode = new Node;
+
+        if (head == nullptr)
+        {
+            head = newNode;
+
+            newNode->data = data;
+            newNode->next = nullptr;
+        }
+        else
+        {
+            newNode->data = data;
+            newNode->next = head;
+            
+            head = newNode;
+        }
+
+        size++;
+    }
+
+    void push_back(T data)
+    {
+        Node* newNode = new Node;
+
+        if (head == nullptr)
+        {
+            head = newNode;
+
+            newNode->data = data;
+            newNode->next = nullptr;
+        }
+        else
+        {
+            currentNode
+
+            newNode->data = data;
+            newNode->next = nullptr;
+
+            head->next = newNode
+        }
+    }
+
+};
+
+int main()
+{
+    SingleLinkedList<int> singleLinkedList;
+
+    singleLinkedList.push_front(10);
+    singleLinkedList.push_front(20);
+    
+    singleLinkedList.push_back(10);
+    singleLinkedList.push_back(20);
+
+    return 0;
+    
+
+
+   // 단일 연결리스트
+   // 
+   //     class SingleLinkedList {};
+   // 
+   //           >   
+   //                   Stack- Heap
+   //     struct Node    head
+   // data  next
+
+    return 0;
+}
