f3380c7c90f2362728c04507d9530e5fecbb8e8a
Gangbin12 <rudqls2023@naver.com>
2025-02-19 19:44:27 +0900

    2025.02.19 (선형 큐)

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,137 +2,58 @@
 
 using namespace std;
 
-template<typename T>
-class DoubleLinkedList
+template <typename T>
+class CircleLinkedList
 {
-private :
+private:
 	struct Node
 	{
 		T data;
-		Node* next;
-		Node* previous;
+		Node * next;
 	};
 
-	Node * head;
-	Node * tail;
-
 	int size;
+	Node* head;
+
 public:
-	DoubleLinkedList()
+	CircleLinkedList()
 	{
-		head = nullptr;
-		tail = nullptr;
 		size = 0;
-	}
-
-	void push_front(T data)
-	{
-		Node* newNode = new Node;
-
-		newNode->data = data;
-		newNode->next = nullptr;
-		newNode->previous = nullptr;
-
-		if (head == nullptr)
-		{
-			head = newNode;
-			tail = newNode;
-		}
-		else
-		{
-			head->previous = newNode;
-			newNode->next = head;
-
-			head = newNode;
-		}
-
-		size++;
-	}
-
-	void pop_front()
-	{
-		if (head == nullptr)
-		{
-			cout << "Linked List is Empty" << endl;
-		}
-		else
-		{
-			Node* deleteNode = head;
-
-			if (head == tail)
-			{
-				head = nullptr;
-				tail = nullptr;
-			}
-			else
-			{
-				deleteNode->next->previous = nullptr;
-
-				head = head->next;
-			}
-
-			delete deleteNode;
-
-			size--;
-		}
+		head = nullptr;
 	}
 
 	void push_back(T data)
 	{
 		Node* newNode = new Node;
 
-		newNode->data = data;
-		newNode->next = nullptr;
-		newNode->previous = nullptr;
+		currentNode;
 
-		if (tail == nullptr)
+		newNode->data = data;
+
+		if(head == nullptr)
 		{
 			head = newNode;
-			tail = newNode;
+
+			newNode->next = head;
 		}
 		else
 		{
-			tail->next = newNode;
-			newNode->previous = tail;
+			newNode->next = head->next;
+			
+			head->next = newNode;
 
-			tail = newNode;
+			head = newNode;
 		}
 
 		size++;
 	}
 
-	void show()
-	{
-		Node* currentNode = head;
-
-		while (currentNode != nullptr)
-		{
-			cout << currentNode->data << " ";
-
-			currentNode = currentNode->next;
-		}
-	}
-
-	~DoubleLinkedList()
-	{
-		while (head != nullptr)
-		{
-			pop_front();
-		}
-	}
+	void 
 };
+
 
 int main()
 {
-	DoubleLinkedList<int> doubleLinkedList;
-
-	doubleLinkedList.push_front(30);
-	doubleLinkedList.push_front(20);
-	doubleLinkedList.push_front(10);
-
-	doubleLinkedList.push_back(40);
-   
-	doubleLinkedList.show();
 
 	return 0;
 }
